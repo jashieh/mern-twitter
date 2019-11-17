201796825bfaee7ed4316f6f337908b48994addd
justin <shieh_justin@yahoo.com>
2019-11-17 15:18:09 -0800

    config for heroku"

--- app.js
+++ app.js
@@ -6,27 +6,35 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const passport = require("passport");
 const app = express();
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
-    .connect(db, { userNewUrlParse: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
-    
+.connect(db, { userNewUrlParse: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-app.get("/", (req, res) => {
-    const user = new User({
-        handle: "justin",
-        email: "justin@gmail.com",
-        password: "password"
-    })
-    user.save();
-    res.send("user created!");
-});
+// app.get("/", (req, res) => {
+//     const user = new User({
+//         handle: "justin",
+//         email: "justin@gmail.com",
+//         password: "password"
+//     })
+//     user.save();
+//     res.send("user created!");
+// });
 
 
 
